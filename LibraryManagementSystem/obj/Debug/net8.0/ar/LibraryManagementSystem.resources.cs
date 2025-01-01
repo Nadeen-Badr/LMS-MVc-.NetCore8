1f797af492bd10a0ca60c0520b4296bf3a2fb1fa
Nadeen-Badr <108485450+Nadeen-Badr@users.noreply.github.com>
2025-01-01 16:54:19 +0200

    intinal styles (custom)

--- LibraryManagementSystem/obj/Debug/net8.0/ar/LibraryManagementSystem.resources.cs
+++ LibraryManagementSystem/obj/Debug/net8.0/ar/LibraryManagementSystem.resources.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute(" ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9289564c3426f6897a101deab8c99253cecd1fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e51ae12c52ada0286ca7948a40ddbac8886ce1e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
