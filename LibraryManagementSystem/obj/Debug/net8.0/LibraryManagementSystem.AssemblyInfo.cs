e51ae12c52ada0286ca7948a40ddbac8886ce1e1
Nadeen-Badr <108485450+Nadeen-Badr@users.noreply.github.com>
2025-01-01 03:03:18 +0200

    translation(done)+all logic done

--- LibraryManagementSystem/obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
+++ LibraryManagementSystem/obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da217521945889e2ed9faa26fb45ef6554cf56fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9289564c3426f6897a101deab8c99253cecd1fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
